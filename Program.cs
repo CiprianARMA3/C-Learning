27b622a25e41aefc9f03da40ae15a86de039aba1
ciprianwindows <stefanoepictyytb@gmail.com>
2026-02-06 20:31:50 +0100

    inheritance

--- Program.cs
+++ Program.cs
@@ -1,20 +1,41 @@
-﻿internal class Persona
-{
-    private string? nome;
-    public string NomeFetched { get { return nome ?? "NULL"; } set { nome = value; } } //return to nome ; set nome = value;
-    private string? testoprivato;
-    public string testo { get { return testoprivato ?? "NULL"; } set { testoprivato = value; } } 
-    
+﻿
+// inheritance in c#
+public class Vehicle {
+
+    public string? Brand { get; set; }
+    public int? Year { get; set; }
+    public Vehicle(string? Brand = null, int? Year = null)
+    {
+        this.Brand = Brand ?? "Brand not given";
+        this.Year = Year;
+    }
+    public void showInfo()
+    {
+        string converted_year = Year.HasValue ? Year.Value.ToString() : "Not given";
+        Console.WriteLine($"Brand : {Brand} && Year : {converted_year}");
+    }
 }
 
-internal class Program
+public class Car : Vehicle {
+    public int? Doors { get; set; }
+    public Car(string? brand= null , int? year=null, int? Doors= null) : base(brand,year){
+        this.Doors = Doors;
+    }
+    public void showInfo()  
+    {
+        string converted_year = Year.HasValue ? Year.Value.ToString() : "Not given";
+        Console.WriteLine($"Brand : {Brand} && Year : {converted_year} && doors : {Doors}");
+    }
+}
+
+
+class Program
 {
     public static void Main(string[] args)
     {
-        Persona ciprian = new Persona();
-        ciprian.NomeFetched = "Ciprian";
-        Console.WriteLine(ciprian.NomeFetched);
-        ciprian.testo = "ciao";
-        Console.WriteLine(ciprian.testo);
+        Car macchina = new Car("BMW",2000,4);
+        macchina.showInfo();
+        
     }
 }
+
