48adba90c999b6419dc2df02c769de033211c49b
ciprianwindows <stefanoepictyytb@gmail.com>
2026-02-06 18:43:23 +0100

    learning classes

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Persona
+{
+    private string? nome;
+    private string? cognome;
+    
+    private Persona(string? nome = null, string? cognome =null)
+    {
+        this.nome = nome ?? "IMP";
+        this.cognome = cognome ?? "IMP";
+    }
+    private void Mostra()
+    {
+        Console.WriteLine($"{nome} / {cognome}");
+    }
+
+    public static void Main(string[] args)
+    {
+        Persona ciprian = new Persona("Ciprian","Mariuta");
+        Persona test = new Persona();
+        ciprian.Mostra();
+        test.Mostra();
+    }
+}
+
+internal class Program
+{
+   // public static void Main(string[] args)
+   // {
+    //    Persona ciprian = new Persona("Ciprian", "Mariuta");
+    //    Persona test = new Persona();
+   //     ciprian.Mostra();
+   //     test.Mostra();
+   //  }
+}
